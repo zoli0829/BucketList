fceae65820ead7dc3dbf225280bdc84f9761fa2c
zoli0829 <zoli0829@gmail.com>
2025-05-06 09:15:53 +0100

    finished day 68

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -7,15 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LoadingView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Text("Loading...")
+    }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("Success!")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("Failed!")
+    }
+}
+
+struct ContentView: View {
+    enum LoadingState {
+        case loading, success, failed
+    }
+    
+    @State private var loadingState = LoadingState.loading
+    
+    var body: some View {
+        switch loadingState {
+        case .loading:
+            LoadingView()
+        case .success:
+            SuccessView()
+        case .failed:
+            FailedView()
         }
-        .padding()
     }
 }
 
